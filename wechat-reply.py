1cbb84fd1b90e2d95c5cb661f3e544cf50ef5519
CaiYansong <caiyansong163@163com>
2025-05-14 13:52:16 +0800

    fix: rm print

--- wechat-reply.py
+++ wechat-reply.py
@@ -50,14 +50,11 @@ while is_running and True:
                 # 目前仅回复最后一条消息（TODO: 判断消息方及是否回复后进行多消息回复）
                 lastMsg = message_list[-1] if message_list else ''
                 lastMsg = lastMsg.Name if lastMsg else ''
-                print(f"最后一条消息 {lastMsg}");
                 # 判断关键字
                 msg = df.apply(lambda x: x['回复内容'] if x['关键词'] in lastMsg else None, axis=1)
                 # 返回的结果是一个包含处理结果的Series对象，msg和列表差不多
-                print(f"匹配到的回复内容：{msg}")
                 msg.dropna(axis=0, how='any', inplace=True)  # 这行代码移除回复内容中的空数据（NaN值）
                 ar = np.array(msg).tolist()  # 这行代码将筛选后的回复内容转换为列表
-                print(f"ar:{ar}")
                 ########### 能够匹配到数据时
                 if ar:
                     # 将数据输入
@@ -66,7 +63,6 @@ while is_running and True:
                     # 发送消息，回车键
                     wx.SendKeys('{Enter}', waitTime=1)
                     # 通过消息匹配检索会话栏的联系人
-                    print(f"回复内容是 {ar[0]}")
                     #wx.TextControl(SubName=ar[0][:5]).RightClick()
                     # break
                 ######### 不能匹配到数据，用机器人回复
